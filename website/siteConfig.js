90d13b60ed772ee837781f47276540c89f8b066e
budiadiono <budiadiono@gmail.com>
2018-12-14 08:51:13 +0700

    added CNAME

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -105,7 +105,9 @@ const siteConfig = {
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  repoUrl: 'https://github.com/anoaland/anoa-cli'
+  repoUrl: 'https://github.com/anoaland/anoa-cli',
+
+  cname: 'anoa.io'
 }
 
 module.exports = siteConfig
